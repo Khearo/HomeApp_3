8657af129cb4b0b5fe5d3a81d290760957487f5a
geekbrains <info@geekbrains.ru>
2020-12-07 23:37:12 +0400

    Add project files.

---
+++ Complex.cs
@@ -0,0 +1,45 @@
+﻿namespace HomeApp_3 // by Aleksandr K
+{
+    public class Complex  // Вынес класс в отдельный файл, как и рекомендовали.
+    {
+        // z=a+bi - выражение вида < - компл число. а - действ, b - мним части.
+        // z1 - z2 = (a1 - a2) + (b1 - b2)i;
+        // z1 * z2= (a1 + b1i) * (a2 + b2i);
+
+        double re; // Действительная часть
+        double im; // Мнимая часть
+
+        public Complex()
+        {
+            re = 0;
+            im = 0;
+        }
+
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;  
+        }
+
+        public Complex Minus(Complex prm)
+        {
+            Complex result = new Complex();
+            result.re = prm.re - re; // Находим значение действительной части
+            result.im = prm.im - im; // Находим значение мнимой части
+            return result;
+        }
+
+        public Complex Mult(Complex complex2)  
+        {
+            return new Complex( this.re * complex2.im + this.im * complex2.re, 
+                                this.re * complex2.re - this.im * complex2.im );
+        }
+
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApp_3 // by Aleksandr K
+{
+    class Program
+    {
+        static void Main()
+        {
+            ComplexClass();   // Вычитание и умножение с помощью Complex. PS, Самое сложное задание для понимания и разбора с начала курса. 
+                              // Также попробовал реализовать через цикл неоднократный ввод выражений, чтобы после 1го не закрывалось окно.     
+            
+            SumOddNumber();  // Через TryParse. 
+        }
+
+        private static void ComplexClass()
+        {
+            Complex complex1 = new Complex(7, 4);
+            Complex complex2 = new Complex(5, 2);
+            Complex result = new Complex();
+
+
+            string operation;
+           
+
+            do
+            {
+                Console.WriteLine("Какое выражение нужно посчитать: minus or multi");
+                operation = Console.ReadLine();
+                switch (operation)
+                
+                {
+                   
+                    case "minus":
+                        result = complex1.Minus(complex2); // Вычитание
+                       
+                        break;
+
+                    case "multi":
+                        result = complex1.Mult(complex2); // Умножение
+                        
+                        break;
+                }
+
+                Console.WriteLine(result.ToString());
+                Console.ReadKey();
+               
+            } while (operation == "minus" || operation == "multi");
+            
+              
+                
+               
+        }
+
+        private static void SumOddNumber()
+        {
+            int number;
+            int sum = 0;
+
+            Console.WriteLine("Введите число: ");
+
+            do
+            {
+                string input = Console.ReadLine();                  // Строковая переменная, которая будет проходить проверку, иначе выдаст сообщение ввести число.
+                bool result = int.TryParse(input, out number);     // Проверяет произошло ли преобразование введенного значения (input) и возвращает его в out. 
+                                                                   //По крайней мере, я так это понимаю. Возможны неточности формулировки.
+
+
+                if (result == true)               
+                {
+                    if (number > 0 && number % 2 == 1)
+                    {
+                        sum = sum + number;
+                    }
+      
+                }
+                else
+                {
+                    Console.WriteLine("Введите пожалуйста число");
+                    Console.ReadKey();
+                }
+            }
+            while (number != 0);
+
+            Console.WriteLine($"Сумма нечетных положительных чисел: {sum}");   
+            Console.ReadKey();
+        }
+    }
+
+}
